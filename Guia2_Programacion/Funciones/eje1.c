eb46bf30306ef09d4d8731c54a2b5dacb211454c
gabriel-inchemet <hernaninchemet10@gmail.com>
2021-11-16 10:15:58 -0300

    update

--- Guia2_Programacion/Funciones/eje1.c
+++ Guia2_Programacion/Funciones/eje1.c
@@ -1,34 +1,30 @@
 /*Realizar un programa que lea los lados a,b,c de un triangulo y Realizar una función que 
-informe si conforman triangulo
+informe si conforman triangulo.
 creador:INCHEMET Gabriel Hernan
 Modificado:
 */
 
 #include<stdio.h>
-int main(void){
-int a, b, c;
-printf("\n Ingrese primer num ");
-scanf("%d",&a);
-setbuf ( stdin, NULL );
-printf("\n Ingrese segundo num ");
-scanf("%d",&b);
-setbuf ( stdin, NULL );
-printf("\n Ingrese tercer num ");
-scanf("%d",&c);
-setbuf ( stdin, NULL );
-if(a==b&&a==c)
-{
-printf("\n El triangulo es equilatero");
-}
-else if(a==b||a==c||b==c)
-{
-printf("\n El triangulo es isosceles");
-}
-else if(a!=b&&a!=b&&b!=c)
-{
-printf("\n El triangulo es escaleno");
-}
-setbuf ( stdin, NULL );
-getchar();
+    int triangulo (int lado1,int lado2,int lado3);
+    int main(){
+    int a, b, c;
+    
+    printf("ingrese valor de lado1");
+    scanf ("%d",&a);
+    printf("ingrese valor de lado2");
+    scanf("%d",&b);
+    printf("ingrese valor de lado3");
+    scanf("%d",&c);
+    triangulo(a,b,c);
+ }
+int triangulo (int lado1,int lado2,int lado3){
 
-}
+     if (((lado1+lado2)>lado3)&&((lado1+lado3)>lado2)&&((lado2+lado3)>lado1))   //a+b>c   a+c>b   b+c>a  formula
+ {
+     printf ("es triangulo");
+ }else{
+    printf("no es triangulo");
+ }
+ 
+ }
+    
